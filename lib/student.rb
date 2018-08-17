c0cd2f745e7f1ec8cdfabaafabe9190b2d7d3dfb
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-17 06:57:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,8 @@ class Student
       INSERT INTO students(name, grade) VALUES (?, ?);
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid()")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    self.id = @id
   end
   
   def create(args)
