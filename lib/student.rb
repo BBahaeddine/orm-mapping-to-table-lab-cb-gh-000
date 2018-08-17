8f101914f99cb7e632c7ac1da5afe6db17636d0a
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-17 06:45:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,10 @@
 class Student
+attr_accessor :name, :grade, :id
 
+def initialize(name, grade)
+  @name = name 
+  @grade = grade
+end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
